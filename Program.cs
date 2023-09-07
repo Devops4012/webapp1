3b1035110e1c0ac86ebca46e55c79d846dd54cb6
SDatta95 <144169952+SDatta95@users.noreply.github.com>
2023-09-07 06:40:34 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello the World!");
+app.MapGet("/", () => "Hello this the World!");
 
 app.Run();
